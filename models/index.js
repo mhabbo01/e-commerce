8458a885354d6eb7252641718c0825e6104d8cf2
Matt Habbo <matthabbo45@gmail.com>
2022-05-07 11:19:33 -0400

    initial code and added columns for the models

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: ''
+})
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: ''
+})
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: '',
+  foreignKey: ''
+})
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: '',
+  foreignKey: ''
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
